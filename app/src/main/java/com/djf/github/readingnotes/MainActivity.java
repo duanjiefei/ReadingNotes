86654e089c81cf332b35b12fe5de72960ee9f87f
duanjiefei <jiefeiduan@163.com>
2018-06-03 17:26:26 +0800

    隐式启动Activity   intent的匹配规则

--- app/src/main/java/com/djf/github/readingnotes/MainActivity.java
+++ app/src/main/java/com/djf/github/readingnotes/MainActivity.java
@@ -10,13 +10,16 @@ import android.widget.Button;
 public class MainActivity extends Activity implements View.OnClickListener{
 
     private Button chapter01;
+    private Button chapter02;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         chapter01 = findViewById(R.id.chapter1);
+        chapter02 = findViewById(R.id.chapter2);
         chapter01.setOnClickListener(this);
+        chapter02.setOnClickListener(this);
     }
 
     @Override
@@ -24,6 +27,10 @@ public class MainActivity extends Activity implements View.OnClickListener{
         Intent intent = new Intent();
         switch (v.getId()){
             case R.id.chapter1:
+                intent.setClassName("com.djf.github.readingnotes", "com.djf.github.readingnotes.chapter01.LifeCycleActivity");
+                startActivity(intent);
+                break;
+            case R.id.chapter2:
                 intent.setClassName("com.djf.github.readingnotes", "com.djf.github.readingnotes.chapter01.LifeCycleActivity");
                 startActivity(intent);
                 break;
--- app/src/main/java/com/djf/github/readingnotes/chapter01/LifeCycleActivity.java
+++ app/src/main/java/com/djf/github/readingnotes/chapter01/LifeCycleActivity.java
@@ -1,9 +1,12 @@
 package com.djf.github.readingnotes.chapter01;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 
-import com.djf.github.readingnotes.Bean.LogUtil;
 import com.djf.github.readingnotes.R;
 import com.djf.github.readingnotes.Utils.SimpleLogUtils;
 
@@ -64,6 +67,17 @@ public class LifeCycleActivity extends Activity {
         SimpleLogUtils.d("onSaveInstanceState");
     }
 
+    /**
+     * 1 当Activity 的启动模式属于singletop时，且该activity位于栈顶时，此方法会被调用
+     * 2 当Actvity 的启动模式属于singletask时，且该activity已经在栈内创建，此方法会被调用
+     * 3 当Activity 的启动模式属于singleinstance时，当activity已经在应用内创建，此方法会被创建
+     * @param intent
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+    }
+
     @Override
     protected void onPause() {
         super.onPause();
@@ -83,5 +97,43 @@ public class LifeCycleActivity extends Activity {
         super.onDestroy();
         //LogUtil.d3("");
         SimpleLogUtils.d("onDestroy");
+    }
+
+
+    /**
+     * 隐式启动Activity的匹配规则
+     *
+     * 1 隐式启动activity 主要通过action category date来进行隐式activity的启动，只有三者都通过了匹配才可以匹配成功
+     *
+     *
+     * a action 的匹配规则
+     *   1  一组intenfilter中可以包含多个action ,那么只要intent可以和intentfilter的一条action 完全匹配，action就可以完成匹配
+     *
+     * b category的匹配规则
+     *  为了使Activity可以响应隐式匹配，必须在category中添加默认的 DEFAULT category,同样只要其中一条的category完成了匹配，就算匹配成功
+     *
+     * c data 的匹配规则
+     *  1 data的组成部分：mimetype 和 URI  默认的mimetype为file或者content
+     *
+     *  2 必须和过滤规则中的一条进行匹配
+     *
+     *
+     * 在真实的开发中，为了避免隐式启动的错误，可以通过intent的resolveActivity方法进行判断
+     *
+     *
+     */
+
+
+    public ComponentName resolveActivity(PackageManager pm,Intent intent) {
+
+        ResolveInfo info = pm.resolveActivity(
+                intent, PackageManager.MATCH_DEFAULT_ONLY);
+        if (info != null) {
+            return new ComponentName(
+                    info.activityInfo.applicationInfo.packageName,
+                    info.activityInfo.name);
+        }
+
+        return null;
     }
 }

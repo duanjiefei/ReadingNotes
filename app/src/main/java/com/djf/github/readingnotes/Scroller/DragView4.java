7c078c13442ea61b7db2efd9dbf2459800f7b4b9
duanjiefei <jiefeiduan@163.com>
2018-06-20 10:14:26 +0800

    利用scroller实现滑动

--- app/src/main/java/com/djf/github/readingnotes/Scroller/DragView4.java
+++ app/src/main/java/com/djf/github/readingnotes/Scroller/DragView4.java
@@ -8,6 +8,8 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Scroller;
 
+import com.djf.github.readingnotes.Utils.SimpleLogUtils;
+
 /**
  * Created by Sky000 on 2018/6/17.
  */
@@ -17,6 +19,7 @@ public class DragView4 extends View {
     private int mLastX;
     private int mLastY;
     private Scroller mScroller;
+    View viewGroup ;
 
     public DragView4(Context context) {
         super(context);
@@ -24,22 +27,27 @@ public class DragView4 extends View {
     }
 
     private void initView(Context context) {
+        SimpleLogUtils.d("initView == ");
         setBackgroundColor(Color.BLACK);
         mScroller = new Scroller(context);
     }
 
     public DragView4(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        initView(context);
     }
 
     public DragView4(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initView(context);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         int x = (int) event.getX();
         int y = (int) event.getY();
+        SimpleLogUtils.d("viewGroup.getScrollX() == "+((View) getParent()).getScrollX());
+        SimpleLogUtils.d("viewGroup.getScrollY() "+((View) getParent()).getScrollX());
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 mLastX = x;
@@ -48,8 +56,13 @@ public class DragView4 extends View {
             case MotionEvent.ACTION_MOVE:
                 int offsetX = x - mLastX;
                 int offsetY = y - mLastY;
+                ((View) getParent()).scrollBy(-offsetX,-offsetY);
                 break;
             case MotionEvent.ACTION_UP:
+                SimpleLogUtils.d("MotionEvent.ACTION_UP");
+                View viewGroup = (View) getParent();
+                mScroller.startScroll(viewGroup.getScrollX(),viewGroup.getScrollY(),-viewGroup.getScrollX(),-viewGroup.getScrollY());
+                invalidate();
                 break;
         }
         return true;
@@ -59,8 +72,13 @@ public class DragView4 extends View {
     @Override
     public void computeScroll() {
         super.computeScroll();
+        SimpleLogUtils.d("mScroller.computeScrollOffset() == "+mScroller.computeScrollOffset());
+        SimpleLogUtils.d("mScroller.getCurrX() == "+mScroller.getCurrX());//((View)getParent()).scrollBy(-offsetX,-offsetY)动作完成之前，偏移量为零，动作结束后才有相应的值
+        SimpleLogUtils.d("mScroller.getCurrY() == "+mScroller.getCurrY());
+        //判断 ((View)getParent()).scrollBy(-offsetX,-offsetY);的动作是否完成
         if (mScroller.computeScrollOffset()){
-
+            ((View)getParent()).scrollTo(mScroller.getCurrX(),mScroller.getCurrY());
+            invalidate();//invalidate > ondraw >computeScroll
         }
     }
 }
